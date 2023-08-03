ca5ae001a803b7c521575ec2448717ebf4aa3544
Hashim Ali Zaidi <62440904+hashimali179@users.noreply.github.com>
2023-08-03 11:54:03 +0530

    make responsive

--- src/components/Checkout/Thank.jsx
+++ src/components/Checkout/Thank.jsx
@@ -2,8 +2,8 @@ import thanks from '../../images/other/thanks1.png'
 import {Link} from 'react-router-dom';
 const Thank = () => {
 	return (
-		<div className="text-center bg-light p-4">
-		<h5 className="">Your order has been received</h5>
+		<div className="text-center bg-light py-4">
+		<p className="fw-600">Your order has been received</p>
 			<img src={thanks} className="img-fluid"/>
 			<p>Thank you for your purchase</p>
 			<p><small className="text-secondary">Your order ID is 152486236</small></p>
